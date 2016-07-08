6bf07a01369a0e879757384feb801ad4af7800d6
Haani Niyaz <haani.niyaz@gmail.com>
2016-07-08 21:16:41 +1000

    Initial commit

---
+++ js/store.js
@@ -0,0 +1,26 @@
+(function(){ 
+	var app = angular.module('store',[]);
+
+	var books = [
+		{
+			name: 'Deep Work',
+			price: 9,
+			desc: 'Rules for focued success in a distracted world',
+			buy: false,
+			soldOut: false
+		},
+		{
+			name: 'The One Thing',
+			price: 9.99,
+			desc: 'The Surprisingly Simple Truth Behind Extraordinary Results',
+			buy: false,
+			soldOut: false
+		}		
+	]
+
+	app.controller('StoreController',function(){
+		this.products = books;	
+	});
+
+
+})();
